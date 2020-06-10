6607476b34a403579f7faefc16d127b766cf7d42
Roger <rmartinez5926.rm@gmail.com>
2020-06-10 12:50:38 -0400

    finish setting up nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+/*=== Dependencies === */
 import React from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import NavB from "./components/navbar"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    
+    <NavB />
   );
 }
 
---
+++ src/components/navbar/index.js
@@ -0,0 +1,24 @@
+/*=== Dependencies === */
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+function NavB() {
+    return (
+        <>
+            <Navbar style={{ backgroundColor: "black" }} variant="dark" expand="lg" sticky="top" collapseOnSelect>
+                <Navbar.Brand>Rogelio Orosco Martinez</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#mywork">My Work</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+
+export default NavB;
