26c3d4108570bb3a728ea708cbade0efe9ab2847
Roger <rmartinez5926.rm@gmail.com>
2020-06-11 09:53:29 -0400

    create about card

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Card from 'react-bootstrap/Card';
 
 function About() {
 
     return (
         <>
-        
+            <Col>
+                <Card>
+                    <Card.Title> A Little Bit About Me</Card.Title>
+                    <Card.Img src="/images/"/>
+                </Card>
+            </Col>
         </>
     )
 }
