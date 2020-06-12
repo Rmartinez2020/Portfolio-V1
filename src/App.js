2c333c19a15f739514aeb8d038cc2fd04e0781a9
Roger <rmartinez5926.rm@gmail.com>
2020-06-12 09:47:08 -0400

    add cover image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
 import NavB from "./components/navbar";
 import About from "./components/about";
 import './App.css';
@@ -12,7 +13,10 @@ function App() {
   return (
     <>
     <NavB />
+    <Image src={process.env.PUBLIC_URL+"/images/cover_image.jpg"} fluid />
     <Container>
+      <Row>
+      </Row>
       <Row>
         <About/>
       </Row>
--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
 import Card from 'react-bootstrap/Card';
-import "./style.css"
+import "./style.css";
 
 function About() {
 
@@ -10,7 +10,12 @@ function About() {
             <Col>
                 <Card className="text-center">
                     <Card.Title> A Little Bit About Me</Card.Title>
-                    <Card.Img src={process.env.PUBLIC_URL+ "/images/profile.png"} style={{borderRadius:"50%", width:"18rem"}}/>
+                    <Card.Img src={process.env.PUBLIC_URL + "/images/profile.png"} style={{ borderRadius: "50%", width: "18rem", margin:"auto" }} />
+                    <Card.Body>
+                        <Card.Text>
+                            Full stack developer with a recently acquired certificate from the University of Central Florida Coding Boot Camp. Technical skills include HTML, CSS, JavaScript,  SQL, and Bootstrap framework. Have a background leading one of the top production lines at a paper mill and working with a team to maximize productivity.  I enjoy solving programming problems and breaking them down to find the best solution to the problem. Ecstatic to start programming with others to  develop captivating innovative web applications.
+                        </Card.Text>
+                    </Card.Body>
                 </Card>
             </Col>
         </>
