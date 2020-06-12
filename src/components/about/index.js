3670a914c7993ed615215991e9224a3da585d49b
Roger <rmartinez5926.rm@gmail.com>
2020-06-12 08:31:12 -0400

    add profile picture

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
 import Card from 'react-bootstrap/Card';
+import "./style.css"
 
 function About() {
 
     return (
         <>
             <Col>
-                <Card>
+                <Card className="text-center">
                     <Card.Title> A Little Bit About Me</Card.Title>
-                    <Card.Img src="/images/"/>
+                    <Card.Img src={process.env.PUBLIC_URL+ "/images/profile.png"} style={{borderRadius:"50%", width:"18rem"}}/>
                 </Card>
             </Col>
         </>
