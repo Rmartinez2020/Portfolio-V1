807ea2e6a8eebf2a4be4620a535d87c6e0ebd728
Roger <rmartinez5926.rm@gmail.com>
2020-06-12 12:04:53 -0400

    create card component for landing page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image';
+import titleCard from "./components/titleCard"
 import NavB from "./components/navbar";
 import About from "./components/about";
 import './App.css';
@@ -12,15 +13,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-    <NavB />
-    <Image src={process.env.PUBLIC_URL+"/images/cover_image.jpg"} fluid />
-    <Container>
-      <Row>
+      <NavB />
+      <Row style={{marginBottom:"150px"}}>
+      <Image src={process.env.PUBLIC_URL + "/images/cover_image.jpg"} fluid />
+      <titleCard />
       </Row>
-      <Row>
-        <About/>
-      </Row>
-    </Container>
+      
+
+      <Container>
+        <Row>
+        </Row>
+        <Row>
+          <About />
+        </Row>
+      </Container>
     </>
   );
 }
---
+++ src/components/titleCard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from 'react-bootstrap/Image'
+
+function titleCard () {
+    return(
+    <div></div>
+    )
+}
+
+export default titleCard;
