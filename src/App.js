8b9dbe54eca26f1608230850252dd0c013930e14
Roger <rmartinez5926.rm@gmail.com>
2020-06-17 11:56:54 -0400

    start making contact component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 import TitleCard from "./components/TitleCard";
 import Projects from "./components/projects";
 import NavB from "./components/navbar";
 import About from "./components/about";
+import Contact from "./components/contact"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,17 +15,20 @@ function App() {
   return (
     <>
       <NavB />
-      <Row style={{marginBottom:"20%"}}>
-      <TitleCard/>
+      <Row style={{ marginBottom: "20%" }}>
+        <TitleCard />
       </Row>
-      
+
 
       <Container>
-        <Row style={{marginBottom:"20%"}}>
+        <Row style={{ marginBottom: "20%" }}>
           <About />
         </Row>
-        <Row style={{marginBottom:"20%"}}>
+        <Row style={{ marginBottom: "20%" }}>
           <Projects />
+        </Row>
+        <Row>
+          <Contact />
         </Row>
       </Container>
     </>
---
+++ src/components/contact/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+
+function Contact () {
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default Contact;
