4222b4ef47990ebd725aa7dff597b23f62d0c656
Roger <rmartinez5926.rm@gmail.com>
2020-06-23 09:30:03 -0400

    add margins

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -9,10 +9,10 @@ function About() {
         <>
             <Col>
                 <Card className="text-center">
-                    <Card.Title> A Little Bit About Me</Card.Title>
+                    <Card.Title style={{fontSize: "30px"}}> A Little Bit About Me</Card.Title>
                     <Card.Img src={process.env.PUBLIC_URL + "/images/profile.png"} style={{ borderRadius: "50%", width: "18rem", margin:"auto" }} />
                     <Card.Body>
-                        <Card.Text>
+                        <Card.Text style={{fontSize: "25px"}}>
                             Full stack developer with a recently acquired certificate from the University of Central Florida Coding Boot Camp. Technical skills include HTML, CSS, JavaScript,  SQL, and Bootstrap framework. Have a background leading one of the top production lines at a paper mill and working with a team to maximize productivity.  I enjoy solving programming problems and breaking them down to find the best solution to the problem. Ecstatic to start programming with others to  develop captivating innovative web applications.
                         </Card.Text>
                     </Card.Body>
--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -4,19 +4,25 @@ function Contact() {
 
     return (
         <>
-        <Col lg={8}>
-        </Col>
+            <Col lg={4}>
+                <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="rogeliooroscomartinez"><a class="LI-simple-link" href='https://www.linkedin.com/in/rogeliooroscomartinez?trk=profile-badge'>Rogelio Orosco Martinez</a></div>
+            </Col>
             <Col>
-                <div style={{backgroundColor:"lightgrey", height:"100%"}}>
-                <div>
-                    <p><span>Email: </span>rmartinez5926.rm@gmail.com</p>
-                </div>
-                <div>
-                    <p><span>Phone: </span>(321) 948-1164</p>
-                </div>
-                <div>
-                    <p><span>Resume: </span><a href="" download>Download A Copy</a></p>
-                </div>
+            </Col>
+            <Col>
+                <div style={{ height: "100%" }}>
+                    <div className="m-5">
+                        <p><span>Email: </span>rmartinez5926.rm@gmail.com</p>
+                    </div>
+                    <div className="m-5">
+                        <p><span>Phone: </span>(321) 948-1164</p>
+                    </div>
+                    <div className="m-5">
+                        <p><span>Resume: </span><a href="" download>Download A Copy</a></p>
+                    </div>
+                    <div className="m-5">
+                        <p><span>Github: </span> <a href="https://github.com/Rmartinez2020" target="_blank" rel="noopener noreferrer">https://github.com/Rmartinez2020</a></p>
+                    </div>
                 </div>
             </Col>
 
