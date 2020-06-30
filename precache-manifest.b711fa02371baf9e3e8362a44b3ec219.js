f6e1f1bce4cc8231899127d9b88f5d809c429582
Roger <rmartinez5926.rm@gmail.com>
2020-06-30 08:42:50 -0400

    Updates

---
+++ precache-manifest.b711fa02371baf9e3e8362a44b3ec219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58c84c3040a749a96a813428db90b42",
+    "url": "/Portfolio-V1/index.html"
+  },
+  {
+    "revision": "8a98cacf20f395cc5fcb",
+    "url": "/Portfolio-V1/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a2565ffe4eb4913bb8bf",
+    "url": "/Portfolio-V1/static/css/main.9adae8a5.chunk.css"
+  },
+  {
+    "revision": "8a98cacf20f395cc5fcb",
+    "url": "/Portfolio-V1/static/js/2.e8c56058.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio-V1/static/js/2.e8c56058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2565ffe4eb4913bb8bf",
+    "url": "/Portfolio-V1/static/js/main.e387f6e2.chunk.js"
+  },
+  {
+    "revision": "99d134985e44d564bab0",
+    "url": "/Portfolio-V1/static/js/runtime-main.8ebfb5c0.js"
+  }
+]);
